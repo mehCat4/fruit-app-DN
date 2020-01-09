ce7c1ded2f3987a5f2e0559fe357871c049af201
Dianne <dianne.numkena@gm.com>
2020-01-09 15:54:10 -0700

    Adding in the version to the bottom of the app

--- fruit-store/src/app/core-components/footer/footer.component.ts
+++ fruit-store/src/app/core-components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'fruit-store-footer',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.less']
 })
 export class FooterComponent implements OnInit {
+  // Variables declared here
+  version = environment.version;
 
   constructor() { }
 
