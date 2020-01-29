a734a76902e6cb971ce8313b215fb7ed75e765b2
Dianne <dianne.numkena@gm.com>
2020-01-29 13:53:31 -0700

    Adding in the fruit search component to the home page

--- fruit-store/src/app/app.module.ts
+++ fruit-store/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FruitDetailsComponent } from './fruit-details/fruit-details.component';
 import {HttpClientModule} from "@angular/common/http";
 import { HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
 import {InMemoryDataService} from "./in-memory-data.service";
+import { FruitSearchComponent } from './fruit-search/fruit-search.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {InMemoryDataService} from "./in-memory-data.service";
     HeaderComponent,
     FooterComponent,
     HomePageComponent,
-    FruitDetailsComponent
+    FruitDetailsComponent,
+    FruitSearchComponent
   ],
   imports: [
     BrowserModule,
